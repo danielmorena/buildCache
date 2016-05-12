0ddc72cc3147c3d6b3f1d2bc19fa9d2d82ebc79f
tnViking <ole-martin@bratteng.me>
2016-05-13 01:12:17 +0200

    Restric users who can access cacheBuilder

--- site/widgets/cacheBuilder/cacheBuilder.php
+++ site/widgets/cacheBuilder/cacheBuilder.php
@@ -1,12 +1,31 @@
 <?php
 
 if (!c::get("panel.widget.cacheBuilder.ignore")) c::set("panel.widget.cacheBuilder.ignore", array("error"));
-if (!c::get("panel.widget.cacheBuilder.jquery")) c::set("panel.widget.cacheBuilder.jquery", u("assets/js/libs/jquery.js"));
-if (!c::get("panel.widget.cacheBuilder.allowed")) c::set("panel.widget.cacheBuilder", array("admin"));
+if (!c::get("panel.widget.cacheBuilder.allowed")) c::set("panel.widget.cacheBuilder.allowed", array("admin"));
+if (!c::get("panel.widget.cacheBuilder.allowed.type")) c::set("panel.widget.cacheBuilder.allowed.type", "role");
 
-return array(
-	"title" => "Cache Builder",
-	"html" => function() {
-		return tpl::load(__DIR__ . DS . "cacheBuilder.html.php");
-	}
-);
+switch (c::get("panel.widget.cacheBuilder.allowed.type")) {
+	case "role":
+		$allowedType = panel()->user()->role();
+		break;
+	case "username":
+		$allowedType = panel()->user()->username();
+		break;
+	case "email":
+		$allowedType = panel()->user()->email();
+		break;
+	default:
+		$allowedType = panel()->user()->role();
+		break;
+}
+
+if(in_array($allowedType, c::get("panel.widget.cacheBuilder.allowed"))):
+	return array(
+		"title" => "Cache Builder",
+		"html" => function() {
+			return tpl::load(__DIR__ . DS . "cacheBuilder.html.php");
+		}
+	);
+else:
+	return false;
+endif;

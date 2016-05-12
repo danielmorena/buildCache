8facfe80eb17cad30028f08ff60872368b4af09e
tnViking <ole-martin@bratteng.me>
2016-05-12 20:22:30 +0200

    Removed jQuery dependency

--- site/widgets/cacheBuilder/cacheBuilder.html.php
+++ site/widgets/cacheBuilder/cacheBuilder.html.php
@@ -1,67 +1,97 @@
 <style>
 #pages{display:-webkit-box;display:-webkit-flex;display:-ms-flexbox;display:flex;-webkit-flex-wrap:wrap;-ms-flex-wrap:wrap;flex-wrap:wrap;-webkit-box-pack:justify;-webkit-justify-content:space-between;-ms-flex-pack:justify;justify-content:space-between}
 #pages .page{background-color:transparent;border:1px solid #ddd;margin-bottom:4px;width:calc(50% - 2px);position:relative;z-index:2}
-#pages .rebuild{background-color:#efefef;border:2px solid #ddd;font-size:1.25rem;line-height:1.75rem;margin-top:10px;text-align:center;width:100%}
-#pages .rebuild:hover{background-color:#ebebeb;cursor:pointer}
 #pages .page span{display:block;padding:3px;position:relative;width:100%}
 #pages .page span .progress{background-color:#efefef;height:100%;position:absolute;top:0;left:0;width:0;z-index:-1;-webkit-transition:width .3s ease;transition:width .3s ease}
+#pages .lang { border-bottom: 2px solid #000; font-weight: bold; margin: 15px 0 10px; width: 100%; }
+#pages #rebuild{background-color:#efefef;border:2px solid #ddd;font-size:1.25rem;line-height:1.75rem;margin-top:10px;text-align:center;width:100%}
+#pages #rebuild:hover{background-color:#ebebeb;cursor:pointer}
 </style>
 
 <div id="pages">
 	<?php
-	// Start listing all the pages
-	foreach(panel()->site()->index() as $key):
-		// If the page is in the ignore list, skip it
-		if (in_array($key, c::get("panel.widget.cacheBuilder.ignore"))) continue; ?>
+	foreach(panel()->site()->languages() as $lang): ?>
+		<div class="lang"><?=$lang->name()?></div>
+		<?php // Start listing all the pages
+		foreach(panel()->site()->index() as $key):
+			// If the page is in the ignore list, skip it
+			if (in_array($key, c::get("panel.widget.cacheBuilder.ignore"))) continue; ?>
 
-	<div class="page" data-name="<?=$key?>">
+	<div class="page" data-name="<?=$key?>" data-lang="<?=$lang?>">
 		<span class="title"><?=$key->title()?> <div class="progress"></div></span>
 	</div>
 
+		<?php endforeach; ?>
 	<?php endforeach; ?>
 
-	<div class="rebuild">Rebuild Cache</div>
+	<div id="rebuild">Rebuild Cache</div>
 </div>
 
-<script src="<?=c::get("panel.widget.cacheBuilder.jquery")?>"></script>
 <script>
-$(document).ready(function() {
-
 	// Crate array of all pages on site
 	var pages = [
 	<?php
-	foreach(panel()->site()->index() as $page):
-		// If the page is in the ignore list, skip it
-		if (in_array($page, c::get("panel.widget.cacheBuilder.ignore"))) continue;
-		echo "{ url: '{$page->url()}', slug: '{$page}'},";
+	$site = panel()->site();
+	foreach ($site->languages() as $lang):
+		foreach($site->index() as $page):
+			// If the page is in the ignore list, skip it
+			if (in_array($page, c::get("panel.widget.cacheBuilder.ignore"))) continue;
+			echo "{ url: '{$site->url()}/{$lang}/{$page}', slug: '{$page}', lang: '{$lang}'},";
+		endforeach;
 	endforeach;
 	?>
 	];
 	var i = 0;
+	var rebuildButton = document.getElementById("rebuild");
+	var progressBars = document.getElementsByClassName("progress");
+	var x = 0;
 
-	function buildCache() {
+
+	// Listen to click
+	rebuildButton.addEventListener("click", function() {
+		rebuildButton.style.backgroundColor = "#efefef";
+		rebuildButton.style.borderColor = "#ddd";
+		rebuildButton.style.color = "#000";
+		// Reset progress bars to 0
+		for(x = 0; x < progressBars.length; x++) { progressBars[x].style.width = "0%"; }
+		i = 0;
+
+		// Purges the cache
+		var xmlhttp = new XMLHttpRequest();
+		xmlhttp.open("GET", "<?=u("purgeCache")?>");
+		xmlhttp.send();
+		buildCache();
+	}, false);
+
+	var buildCache = function() {
 		if (i < pages.length) {
-			$("[data-name='"+ pages[i].slug +"'] .progress").css("width", "75%");
-			$.ajax({
-				cache: false,
-				url: pages[i].url,
-				success: function() {
-					$("[data-name='"+ pages[i].slug +"'] .progress").css("width", "100%");
+			// Select progressbar by it's parent's [data-name]
+			var pageProgress = document.querySelector("[data-name='"+ pages[i].slug +"'][data-lang='"+ pages[i].lang +"'] .progress");
+			// Set progress bar to 75%;
+			pageProgress.style.width = "75%";
+
+			// Ready a new request
+			var xmlhttp = new XMLHttpRequest();
+
+			// POST makes sure it doesn't cache itself
+			xmlhttp.open("POST", pages[i].url);
+			xmlhttp.send();
+			xmlhttp.onreadystatechange = function() {
+				// Only fire when the POST call has returned
+				if ((xmlhttp.readyState == 4) && (xmlhttp.status == 200)) {
+					// Set progress bar to 100%;
+					pageProgress.style.width = "100%";
 					i++;
-					buildCache();	
+
+					// Proceed to next
+					buildCache();
 				}
-			});
+			}
+		}
+		if (i == pages.length) {
+			rebuildButton.style.backgroundColor = "#8dae28";
+			rebuildButton.style.borderColor = "#8dae28";
+			rebuildButton.style.color = "#fff";
 		}
 	}
-
-	$(".rebuild").click(function() {
-		// Reset progress bars to 0
-		$(".progress").css("width", "0%");
-		i = 0;
-		// Purges the cache
-		$.get("<?=u("purgeCache")?>");
-		// Builds the cache
-		buildCache();
-	});
-});
 </script>

48c019f7ad273cb67fb8ae868d356ee6a63a74ba
tnViking <ole-martin@bratteng.me>
2016-05-13 01:11:51 +0200

    Restric users who can access cacheBuilder

--- site/widgets/cacheBuilder/cacheBuilder.php
+++ site/widgets/cacheBuilder/cacheBuilder.php
@@ -2,6 +2,7 @@
 
 if (!c::get("panel.widget.cacheBuilder.ignore")) c::set("panel.widget.cacheBuilder.ignore", array("error"));
 if (!c::get("panel.widget.cacheBuilder.jquery")) c::set("panel.widget.cacheBuilder.jquery", u("assets/js/libs/jquery.js"));
+if (!c::get("panel.widget.cacheBuilder.allowed")) c::set("panel.widget.cacheBuilder", array("admin"));
 
 return array(
 	"title" => "Cache Builder",
